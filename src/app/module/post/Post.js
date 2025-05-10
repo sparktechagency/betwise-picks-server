5e6c10ccfad68e095257597e2f4a9bee9234086d
ThakurSaad <thakursaad613@gmail.com>
2025-05-10 12:16:54 +0600

    post model done

--- src/app/module/post/Post.js
+++ src/app/module/post/Post.js
@@ -1,8 +1,57 @@
 const { Schema, model } = require("mongoose");
+const { EnumSubscriptionPlan } = require("../../../util/enum");
 const ObjectId = Schema.Types.ObjectId;
 
 const postSchema = new Schema(
-  {},
+  {
+    postTitle: {
+      type: String,
+      required: true,
+    },
+    sportType: {
+      type: String,
+      required: true,
+    },
+    predictionType: {
+      type: String,
+      required: true,
+    },
+    predictionDescription: {
+      type: String,
+      required: true,
+    },
+    winRate: {
+      type: Number,
+      required: true,
+    },
+    TargetUser: {
+      type: String,
+      required: true,
+      enum: {
+        values: [
+          EnumSubscriptionPlan.BRONZE,
+          EnumSubscriptionPlan.SILVER,
+          EnumSubscriptionPlan.GOLD,
+        ],
+        message: `Invalid Target user status. Allowed values: ${Object.values(
+          EnumSubscriptionPlan
+        ).join(", ")}`,
+      },
+    },
+    oddsRange: {
+      type: String,
+      required: true,
+    },
+    post_image: {
+      type: String,
+      required: true,
+    },
+    postedBy: {
+      type: ObjectId,
+      ref: "Admin",
+      required: true,
+    },
+  },
   {
     timestamps: true,
   }
