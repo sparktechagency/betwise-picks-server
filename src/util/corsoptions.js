b012a5a9f87d92777f08490c02e70e7378a2a96d
ThakurSaad <thakursaad613@gmail.com>
2025-06-19 15:12:35 +0600

    more cors added

--- src/util/corsoptions.js
+++ src/util/corsoptions.js
@@ -1,5 +1,10 @@
 const corsOptions = {
-  origin: ["http://10.0.60.24:8000"],
+  origin: [
+    "http://10.0.60.24:8000", // local server
+    "http://18.158.237.149:8001", // live server
+    "http://18.158.237.149:8002", // live website
+    "http://18.158.237.149:8003", // live dashboard
+  ],
   credentials: true,
 };
 
